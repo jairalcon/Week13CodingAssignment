c6ade3450bd75b654b5b7b5745cb82c9874eca61
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-08-26 18:46:19 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import '../node_modules/jquery/dist/jquery.js';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js';
+// import '../node_modules/jquery/dist/jquery.js';
+import 'https://code.jquery.com/jquery-3.6.1.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
