a936b41fbb4b65397fdfb855348252eff558ba7a
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-08-26 18:31:54 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
-// import LoginForm from './components/login-form';
-import MainPage from './components/main-page';
-// import NavBar from './components/nav-bar';
+import logo from './logo.svg';
+import './App.css';
 
-
-export default class App extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <div className='row'>
-          <MainPage/>
-        </div>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
