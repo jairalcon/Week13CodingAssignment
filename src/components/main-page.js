b050124a40cedebb5c336a76ad488c387aee2380
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-12-08 14:43:30 -0600

    updating

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import NavBar from './nav-bar';
-import LoginForm from './login-form';
+import React, { Component } from 'react'
+import NavBar from './nav-bar'
+import LoginForm from './login-form'
 
 export default class MainPage extends Component {
-    render() {
-        return (
-            <>
-            <NavBar/>
-            <LoginForm/>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <NavBar />
+        <LoginForm />
+      </>
+    )
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css';
-// import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js';
-// import '../node_modules/jquery/dist/jquery.js';
-import 'https://code.jquery.com/jquery-3.6.1.min.js';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+// import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/jquery/dist/jquery.js'
+// import 'https://code.jquery.com/jquery-3.6.1.min.js'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
